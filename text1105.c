230a5b60ca0be20a3bd9e02eb61d67f6a497841e
unknown <2054910582@qq.com>
2019-11-09 23:12:11 +0800

    https://github.com/zhoujiaqi0825/03.c.git

--- text1105.c
+++ text1105.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
-#include<math.h>
 int main (void)
 {
-    int money,yaer;
-    double rate,sum;
-
-    printf("Enter money\n ");
+    int a;
+    printf("请输入一个整数");
+    scanf("%d",&a);
+    if(a>=90){
+        printf("A\n");
+    }else if(a<90 && a>=80){
+        printf("B\n");
+    }else if(a<80 && a>=70){
+        printf("C\n");
+    }else if(a<70 && a>=60){
+        printf("D\n");
+    }else
+    {
+        printf("E\n");
+    }
+    return 0;
+}     
