c7b06f4ca7fbfd5d9708b72b32a4395f2b9cf367
unknown <2054910582@qq.com>
2019-11-12 12:50:41 +0800

    https://github.com/zhoujiaqi0825/03.c.git

---
+++ text1111.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main (void)
+{
+    int choice,i;
+    double price;
+
+    for(i=1;i<=5;i++){
+        printf("[1]Select apples\n");
+        printf("[2]Select pears\n");
+        printf("[3]Select oranges\n");
+        printf("[4]Select grapes\n");
+        printf("[0]exit \n");
+
+        printf("Enter choice:");
+        scanf("%d",&choice);
+        if(choice==0)
+          break;
+        switch(choice){
+          case 1:price=3.0;break;
+          case 2:price=2.5;break;
+          case 3:price=4.1;break;
+          case 4:price=10.2;break;
+          default:price=0.0;break;
+        }
+        printf("price is %.1f\n",price);
+    }
+    printf("Thanks \n"); 
+
+    return 0;   
+}
+    
